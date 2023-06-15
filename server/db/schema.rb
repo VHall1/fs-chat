7013dcebefce9f0c0906b6d41d1d898eeb4caf1d
VHall1 <victorhallpsn@hotmail.com>
2023-06-15 01:02:59 +0100

    add active flag to control users

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_14_232251) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_15_000210) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_232251) do
     t.integer "discriminator", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "active", default: false
   end
 
   add_foreign_key "messages", "users"
