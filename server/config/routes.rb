1e4a3b33057ec1d9cd931ee69d5d00b4b19a41e5
VHall1 <victorhallpsn@hotmail.com>
2023-06-19 15:07:37 +0100

    fix: update routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :messages
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :auth do
+    get :authenticate
+    get :me
+  end
 end
