a52bb38750a12096383ce71390f2cf16735f988d
VHall1 <victorhallpsn@hotmail.com>
2023-07-18 22:59:02 +0100

    use vite prefix for client env variables

--- client/src/api.ts
+++ client/src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import ActionCable from "actioncable";
 
 export const api = axios.create({
-  baseURL: import.meta.env["API_URL"] || "http://localhost:3000",
+  baseURL: import.meta.env["VITE_API_URL"] || "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
@@ -10,5 +10,5 @@ export const api = axios.create({
 });
 
 export const wss = ActionCable.createConsumer(
-  import.meta.env["WS_URL"] || "ws://localhost:3000/cable"
+  import.meta.env["VITE_WS_URL"] || "ws://localhost:3000/cable"
 );
