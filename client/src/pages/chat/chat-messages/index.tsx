744cfdb7d4004b124f363cac5b94518994f51781
VHall1 <victorhallpsn@hotmail.com>
2023-07-18 21:35:17 +0100

    debounce fetchOnScroll function

--- client/src/pages/chat/chat-messages/index.tsx
+++ client/src/pages/chat/chat-messages/index.tsx
@@ -4,6 +4,7 @@ import {
   QueryFunction,
   useInfiniteQuery,
 } from "@tanstack/react-query";
+import { debounce } from "lodash";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { api, wss } from "../../../api";
 import { ChatInput } from "../chat-input";
@@ -31,7 +32,7 @@ export const ChatMessages = () => {
             pages: [
               {
                 ...prev.pages[0],
-                data: [...data, ...prev.pages[0].data],
+                data: [JSON.parse(data), ...prev.pages[0].data],
               },
               ...prev.pages.slice(1),
             ],
@@ -45,7 +46,7 @@ export const ChatMessages = () => {
     };
   }, [setMessages]);
 
-  const { isLoading, isFetching, fetchNextPage } = useInfiniteQuery({
+  const { isLoading, isFetchingNextPage, fetchNextPage } = useInfiniteQuery({
     queryKey: ["messages"],
     queryFn: getMessages,
     getNextPageParam: (lastPage) => lastPage.nextCursor,
@@ -58,28 +59,34 @@ export const ChatMessages = () => {
     [messages]
   );
 
-  const hasOldestMessage = messages?.pages?.slice(-1)[0].nextCursor === 1;
+  const hasOldestMessage = messages?.pages?.slice(-1)[0].nextCursor === null;
   const fetchMoreOnTopReached = useCallback(
     (containerRefElement?: HTMLDivElement | null) => {
       if (containerRefElement) {
         const { scrollHeight, scrollTop, clientHeight } = containerRefElement;
         //once the user has scrolled within 300px of the bottom of the table, fetch more data if there is any
         if (
-          scrollHeight + scrollTop - clientHeight < 300 &&
-          !isFetching &&
-          !hasOldestMessage
+          !isFetchingNextPage &&
+          !hasOldestMessage &&
+          scrollHeight + scrollTop - clientHeight < 300
         ) {
           fetchNextPage();
         }
       }
     },
-    [isFetching, fetchNextPage, hasOldestMessage]
+    [isFetchingNextPage, fetchNextPage, hasOldestMessage]
+  );
+  const debouncedFetchMoreOnTopReached = useCallback(
+    debounce(fetchMoreOnTopReached, 250),
+    [fetchMoreOnTopReached]
   );
 
   return (
     <Paper sx={{ height: 1.0, display: "flex", flexDirection: "column" }}>
       <MessagesContainer
-        onScroll={(e) => fetchMoreOnTopReached(e.target as HTMLDivElement)}
+        onScroll={(e) =>
+          debouncedFetchMoreOnTopReached(e.target as HTMLDivElement)
+        }
       >
         {isLoading && <Typography>Loading...</Typography>}
         {flatMessages.map((message) => (
