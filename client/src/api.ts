bc15a21cdded776d22139eb6367a14617a97d51d
VHall1 <victorhallpsn@hotmail.com>
2023-07-18 22:46:39 +0100

    use env variables on client

--- client/src/api.ts
+++ client/src/api.ts
@@ -2,11 +2,13 @@ import axios from "axios";
 import ActionCable from "actioncable";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env["API_URL"] || "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
   withCredentials: true,
 });
 
-export const wss = ActionCable.createConsumer("ws://localhost:3000/cable");
+export const wss = ActionCable.createConsumer(
+  process.env["WS_URL"] || "ws://localhost:3000/cable"
+);
